4ccd7e80ec52b3def66a35a953b0146e4837d4bf
BozhidarY <b1j2d3a4r@gmail.com>
2023-11-15 12:46:18 +0200

    new JwTAuthenticationConverter

--- Spring-Music-App/src/main/java/com/example/demo/SpringSecurity/SecurityConfiguration.java
+++ Spring-Music-App/src/main/java/com/example/demo/SpringSecurity/SecurityConfiguration.java
@@ -1,12 +1,6 @@
 package com.example.demo.SpringSecurity;
 
-//import com.example.demo.Utils.RSAKeyProperties;
-//import com.nimbusds.jose.jwk.JWK;
-//import com.nimbusds.jose.jwk.JWKSet;
-//import com.nimbusds.jose.jwk.RSAKey;
-//import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-//import com.nimbusds.jose.jwk.source.JWKSource;
-//import com.nimbusds.jose.proc.SecurityContext;
+
 import com.example.demo.Utils.RSAKeyProperties;
 import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
@@ -33,11 +27,7 @@ import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.oauth2.jwt.JwtDecoder;
-//import org.springframework.security.oauth2.jwt.JwtEncoder;
-//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-//import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfiguration {
